2505acdb8dc5f947d063402e655ecf0ef8d7c9c5
jromer <j.p.l.romer@student.tudelft.nl>
2023-12-04 01:36:53 +0100

    day 2

--- day2/day2.py
+++ day2/day2.py
@@ -7,18 +7,18 @@ def getCubes(color: str, line: str) -> int:
     regex = r'(\d+) '
     return max(map(int, re.findall(f'{regex}{color}', line)))
 
-def game_is_possible(line) -> int:
+def game_is_possible(line: str) -> int:
     y = line.split(': ')
     ID = int(re.findall(r'\d+', y[0])[0])
     x = y[1]
-    maximums = [('red', 12), ('green', 13), ('blue', 12)]
+    maximums = [('red', 12), ('green', 13), ('blue', 14)]
     if any(getCubes(color, x) > m for color, m in maximums):
         return 0
     else:
         return ID
 
-def power(line) -> int:
-    return reduce(mul, map(partial(getCubes, line=line), ['red', 'blue', 'green']))
+def power(line: str) -> int:
+    return reduce(mul, map(partial(getCubes, line=line), ['red', 'green', 'blue']))
 
 with open('input') as f:
     lines = f.read().splitlines()
