ae7b505875587f1529e32efbdb5ccd90e93ce08f
jromer <j.p.l.romer@student.tudelft.nl>
2023-12-04 19:05:07 +0100

    day 4

--- day4/day4.py
+++ day4/day4.py
@@ -1,34 +1,31 @@
 #!/bin/python
 import re, math
+from typing import NamedTuple
+
+Card = NamedTuple('Card', [('index', int), ('wins', int)])
 
 def parseNums(line: str) -> list[int]:
     return list(map(int, re.findall(r'\d+', line)))
 
 def getWinningNums(line: str) -> int:
-    winningS, mineS = line.split(': ')[1].split('|')
-    winning = parseNums(winningS)
-    mine = parseNums(mineS)
-    return sum([int(n in mine) for n in winning])
+    winning, mine = map(parseNums, line.split(': ')[1].split('|'))
+    return sum(int(n in mine) for n in winning)
 
-def partone(line: str) -> int:
-    power = getWinningNums(line) - 1
-    if power == -1:
-        return 0
-    else:
-        return int(math.pow(2, power))
+def cardToPoints(line: str) -> int:
+    exponent = getWinningNums(line) - 1
+    return 0 if exponent == -1 else int(math.pow(2, exponent))
 
-def parttwo(lines: list[str]):
-    cards = list(enumerate([getWinningNums(x) for x in lines]))
-    amounts = [1 for _ in cards]
-    for i in range(len(cards)):
-        for _ in range(amounts[cards[i][0]]):
-            for j in range(cards[i][1]):
-                idx = cards[i][0] + j + 1
-                amounts[idx] += 1
+def getAmountOfScratchcards(lines: list[str]):
+    cards = [Card(*x) for x in enumerate(map(getWinningNums, lines))]
+    amounts = [1] * len(cards)
+    for card in cards:
+        for i in range(card.wins):
+            index = card.index + i + 1
+            amounts[index] += amounts[card.index]
     return sum(amounts)
 
 with open('input') as f:
     lines = f.read().splitlines()
-    part1 = sum(map(partone, lines))
-    part2 = parttwo(lines)
+    part1 = sum(map(cardToPoints, lines))
+    part2 = getAmountOfScratchcards(lines)
     print(part1, part2)
